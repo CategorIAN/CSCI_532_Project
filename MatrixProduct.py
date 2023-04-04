402a675fc32ecaf13420077efdad65f00989fc63
CategorIAN <ianholm9@gmail.com>
2023-04-04 07:59:21 -0600

    ..

--- MatrixProduct.py
+++ MatrixProduct.py
@@ -39,28 +39,36 @@ class MatrixProduct:
             return self.matDims[i][0] * self.matDims[k][1] * self.matDims[j][1]
         return f
 
-    def deltaStates(self, state, action):
-        (i, k, j) = (state[0], action, state[1])
-        return {(i, k), (k + 1, j)}
+    def deltaStates(self, state, scope):
+        (i, j) = state
+        def f(action):
+            k = action
+            return {(i, k), (k + 1, j)} if scope is None else {(i, k), (k + 1, j)}.intersection(scope)
+        return lambda a: set() if i == j else f
 
-    def actionValue(self, state):
+    def actionValue(self, state, scope = None):
         v = self.v(state)
         def f(action):
-            return sum([self.dynamic(s) for s in self.deltaStates(state, action)]) + v(action)
-        return f
+            return sum([self.dynamic(s) for s in self.deltaStates(state, scope)(action)]) + v(action)
+        return lambda a: 0 if state[0] == state[1] else f
 
-    def optPair(self, state):
+    def optPair(self, state, scope = None):
         if state[0] == state[1]:
             return (None, 0)
         else:
-            valueFunc = self.actionValue(state)
+            valueFunc = self.actionValue(state, scope)
             minPair = lambda av, bv: av if av[1] <= bv[1] else bv
             return reduce(minPair, [(a, valueFunc(a)) for a in self.actionSpace(state)])
 
+    def optAction(self, state, scope = None):
+        return self.optPair(state, scope)[0]
+
+    def optValue(self, state, scope = None):
+        return self.optPair(state, scope)[1]
+
     def optMults(self):
-        for (i, j) in self.stateSpace():
-            (_, minMults) = self.optPair((i, j))
-            self.table[i, j] = minMults
+        for (i, j) in self.stateSpace:
+            self.table[i, j] = self.optValue((i, j))
         return self.dynamic((0, self.n - 1))
 
     #==============================================================================================================#
@@ -68,16 +76,18 @@ class MatrixProduct:
     def featVec(self, state):
         return np.array([state[0], state[1]])
 
-    def netData(self, states):
+    def dataFrame(self, states):
         (iList, jList) = [list(t) for t in list(zip(*states))]
         return pd.DataFrame({"i": iList, "j": jList})
 
-    def run(self, nn, D, eps, eta):
-        (S, V) = (set(), {self.finalState})
-        Q = set(random.sample(self.stateSpace, 5))|{self.finalState}
+    def trainData(self, nn, D, eps, eta):
+        (S, V, Q) = (set(), {self.finalState}, {self.finalState})
         def go(D, S, V, Q):
             if len(Q) == 0:
-                return self.netData(set(random.sample(D, 5))|S)
+                return self.dataFrame(set(random.sample(D, 5))|S)
+            else:
+                state = Q[0]
+                action = random.choice(self.actionSpace(state)) if random.random() < eps else self.optAction(state)
 
 
 
