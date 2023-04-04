6c48a885e118d94d3db95f8dc8a6cc2f24ec76e5
CategorIAN <ianholm9@gmail.com>
2023-04-04 11:52:33 -0600

    Working on Neural Net.

--- MatrixProduct.py
+++ MatrixProduct.py
@@ -9,7 +9,7 @@ class MatrixProduct:
             self.matDims = dimensionList
             self.n = len(dimensionList)
             self.table = np.vectorize(lambda n: int(n))(np.zeros((self.n, self.n)))
-            self.dynamic = lambda state: self.table[state[0], state[1]]
+            self.dynamicStd = lambda state: self.table[state[0], state[1]]
             self.stateSpace = self.getStateSpace()
             self.finalState = (0, self.n - 1)
         else:
@@ -39,55 +39,59 @@ class MatrixProduct:
             return self.matDims[i][0] * self.matDims[k][1] * self.matDims[j][1]
         return f
 
-    def deltaStates(self, state, scope):
+    def deltaStates(self, state, D = None):
         (i, j) = state
         def f(action):
             k = action
-            return {(i, k), (k + 1, j)} if scope is None else {(i, k), (k + 1, j)}.intersection(scope)
+            return {(i, k), (k + 1, j)} if D is None else {(i, k), (k + 1, j)}.intersection(D)
         return lambda a: set() if i == j else f
 
-    def actionValue(self, state, scope = None):
+    def actionValue(self, state, D = None):
+        dynamic = self.dynamicStd if D is None else lambda s: D[s]
         v = self.v(state)
         def f(action):
-            return sum([self.dynamic(s) for s in self.deltaStates(state, scope)(action)]) + v(action)
+            return sum([dynamic(s) for s in self.deltaStates(state, D)(action)]) + v(action)
         return lambda a: 0 if state[0] == state[1] else f
 
-    def optPair(self, state, scope = None):
+    def optPair(self, state, D = None):
         if state[0] == state[1]:
             return (None, 0)
         else:
-            valueFunc = self.actionValue(state, scope)
+            valueFunc = self.actionValue(state, D)
             minPair = lambda av, bv: av if av[1] <= bv[1] else bv
             return reduce(minPair, [(a, valueFunc(a)) for a in self.actionSpace(state)])
 
-    def optAction(self, state, scope = None):
-        return self.optPair(state, scope)[0]
+    def optAction(self, state, D = None):
+        return self.optPair(state, D)[0]
 
-    def optValue(self, state, scope = None):
-        return self.optPair(state, scope)[1]
+    def optValue(self, state, D = None):
+        return self.optPair(state, D)[1]
 
     def optMults(self):
         for (i, j) in self.stateSpace:
             self.table[i, j] = self.optValue((i, j))
-        return self.dynamic((0, self.n - 1))
+        return self.dynamicStd((0, self.n - 1))
 
     #==============================================================================================================#
 
-    def featVec(self, state):
-        return np.array([state[0], state[1]])
+    def filter(self, condition, states):
+        states_filtered = [[s] if condition(s) else [] for s in states]
+        return reduce(lambda l1, l2: l1 + l2, states_filtered, [])
 
     def dataFrame(self, states):
         (iList, jList) = [list(t) for t in list(zip(*states))]
         return pd.DataFrame({"i": iList, "j": jList})
 
-    def trainData(self, nn, D, eps, eta):
-        (S, V, Q) = (set(), {self.finalState}, {self.finalState})
-        def go(D, S, V, Q):
+    def trainData(self, D, eps):
+        def go(S, V, Q):
             if len(Q) == 0:
-                return self.dataFrame(set(random.sample(D, 5))|S)
+                return self.dataFrame(set(random.sample(D.keys(), 5))|S)
             else:
                 state = Q[0]
-                action = random.choice(self.actionSpace(state)) if random.random() < eps else self.optAction(state)
+                action = random.choice(self.actionSpace(state)) if random.random() < eps else self.optAction(state, D)
+                newStates = self.filter(lambda s: s not in V, self.deltaStates(state)(action))
+                return go(S|{state}, V|set(newStates), Q + newStates)
+        return go(set(), {self.finalState}, {self.finalState})
 
 
 
--- NeuralNet.py
+++ NeuralNet.py
@@ -1,8 +1,18 @@
 import numpy as np
 from functools import reduce
+import random
 
 
 class NeuralNet:
+    def __init__(self, data):
+        self.data = data
+
+    def featVec(self, state):
+        return np.array([state[0], state[1]])
+
+    def targetvec(self):
+        return lambda s: self.data[s]
+
     def randomWeights(self, rowDim, colDim):
         return np.random.rand(rowDim, colDim)
 
@@ -10,3 +20,9 @@ class NeuralNet:
         appendWeight = lambda wL, dim: wL + [self.randomWeights(dim, wL[-1].shape[0])]
         dims = [featNum] + hidLayers + [targetNum]
         return reduce(appendWeight, dims[2:], [self.randomWeights(*reversed(dims[0:-1:]))])
+
+    def stochastic_online_gd(self):
+        vec_func = self.featVec  # create vector function for data
+        base_index = random.sample(self.data.keys())  # create a shuffled index for iteration
+                   # target length needs to be number of classes
+        r = self.targetvec()
